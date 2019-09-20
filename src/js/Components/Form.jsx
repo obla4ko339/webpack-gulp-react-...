43d3272ef591e0d961230287ba75e1f1ef933618
Bullet <volodya.grab@yandex.ru>
2019-09-20 11:25:20 +0500

    Collector webpack+gulp+react

---
+++ src/js/Components/Form.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+
+export default class Form extends React.Component{
+
+    render(){
+        return(
+            <div>
+                <h1>Hello Kity</h1>
+                <div>
+                    New block
+                </div>
+            </div>   
+        )
+    }
+
+}
